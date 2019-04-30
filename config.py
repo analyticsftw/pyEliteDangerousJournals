d5feb2c09780429cdfa6997eff224746d82f4727
Julien Coquet <julien.coquet@gmail.com>
2019-04-30 13:34:45 +0200

    Added config.py

---
+++ config.py
@@ -0,0 +1,39 @@
+# Settings 
+#    Replace misc settings here such as Journal file locations
+#    On a PC: \Users\yourname\Saved Games\Frontier Developments\Elite Dangerous
+#    On a Mac: /Users/yourname/Library/Application Support/Frontier Developments/Elite Dangerous
+#
+journalSettings = dict(
+    path = "journals",
+    ships = [
+        {"name":"adder", "realName": "Adder","kills": 0,"bounty": 0},
+        {"name":"anaconda", "realName": "Anaconda","kills": 0,"bounty": 0},
+        {"name":"asp", "realName": "ASP Explorer","kills": 0,"bounty": 0},
+        {"name":"asp_scout", "realName": "ASP Scout","kills": 0,"bounty": 0},
+        {"name":"cobramkiii", "realName": "Cobra MkIII","kills": 0,"bounty": 0},
+        {"name":"cobramkiv", "realName": "Cobra MkIV","kills": 0,"bounty": 0},
+        {"name":"diamondback", "realName": "Diamondback Scout","kills": 0,"bounty": 0},
+        {"name":"diamondbackxl", "realName": "Diamondback Explorer","kills": 0,"bounty": 0},
+        {"name":"eagle", "realName": "Eagle","kills": 0,"bounty": 0},
+        {"name":"empire_courier", "realName": "Imperial Courier","kills": 0,"bounty": 0},
+        {"name":"empire_eagle", "realName": "Imperial Eagle","kills": 0,"bounty": 0},
+        {"name":"empire_trader", "realName": "Imperial Clipper","kills": 0,"bounty": 0},
+        {"name":"federation_corvette", "realName": "Federal Corvette","kills": 0,"bounty": 0},
+        {"name":"federation_dropship", "realName": "Federal Dropship","kills": 0,"bounty": 0},
+        {"name":"federation_dropship_mkii", "realName": "Federal Assault Ship","kills": 0,"bounty": 0},
+        {"name":"federation_gunship", "realName": "Federal Gunship","kills": 0,"bounty": 0},
+        {"name":"ferdelance", "realName": "Fer-De-Lance","kills": 0,"bounty": 0},
+        {"name":"generic ship", "realName": "No ship model","kills": 0,"bounty": 0},
+        {"name":"hauler", "realName": "Hauler","kills": 0,"bounty": 0},
+        {"name":"independant_trader", "realName": "Independent trader (lol)","kills": 0,"bounty": 0},
+        {"name":"orca", "realName": "Orca","kills": 0,"bounty": 0},
+        {"name":"python", "realName": "Python","kills": 0,"bounty": 0},
+        {"name":"sidewinder", "realName": "Sidewinder","kills": 0,"bounty": 0},
+        {"name":"type6", "realName": "Type-6 Transport","kills": 0,"bounty": 0},
+        {"name":"type7", "realName": "Type-7 Transport","kills": 0,"bounty": 0},
+        {"name":"type9", "realName": "Type-9 Transport","kills": 0,"bounty": 0},
+        {"name":"viper", "realName": "Viper MkIII","kills": 0,"bounty": 0},
+        {"name":"viper_mkiv", "realName": "Viper MkIV","kills": 0,"bounty": 0},
+        {"name":"vulture", "realName": "Vulture","kills": 0,"bounty": 0}
+    ]
+)
